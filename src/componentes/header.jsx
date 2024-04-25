47f2518e6f71f0b86e523225ca290b703329471b
wei <miguelxu124@.gmail.com>
2024-04-25 15:14:48 +0200

    página principal

--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -4,6 +4,16 @@ function Header() {
     return(
         <header>
             <img src="" alt="" />
+            <div>
+                <div><a href="">Games</a></div>
+                <div><a href="">Tienda</a></div>
+                <div><a href="">Clasificación y Noticias</a></div>
+                <div><a href="">Torneos</a></div>
+            </div>
+            <div>
+                <div>Inicia Session</div>
+                <div>Registro</div>
+            </div>
         </header>
     )
 }
--- src/componentes_index/noticias.jsx
+++ src/componentes_index/noticias.jsx
@@ -1,70 +1,50 @@
 import "./noticias.css"
 import { useGSAP } from "@gsap/react";
+import { motion } from "framer-motion";
 import gsap from "gsap";
 import {ScrollTrigger} from "gsap/ScrollTrigger";
-import { useState} from "react";
 function Noticias() {
   gsap.registerPlugin(ScrollTrigger);
-
-  const spans = useState(Array(6).fill("Notícia"));
-  // const spanRef = useRef(null);
-
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     if (spanRef.current) { // 确保 spanRef.current 不为空
-  //       const rect = spanRef.current.getBoundingClientRect();
-  //       if (rect.right <= 0) {
-  //         console.log(1);
-  //         setSpans(prevSpans => prevSpans.filter(span =>
-  //           {
-  //             if (span != spanRef.current) {
-                
-  //               return console.log(1); // 保留当前元素
-  //             } else {
-  //               console.log(0); // 输出信息
-  //               return false; // 过滤掉当前元素
-  //             }
-  //           }));
-  //       }
-  //     }
-  //   };
-  
-  //   window.addEventListener('scroll', handleScroll);
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //   };
-  // }, []);
-  
-
-
     useGSAP(()=>{
         ScrollTrigger.create({
             trigger: ".noticias",
             markers: true,
             start: "0% 100%",
-            end: "100% 100%",
-            scrub:true,
-            // toggleActions: "play none none",
+            end: "0% 100%",
+            // scrub:true,
+            toggleActions: "restart none none",
             animation: 
-            gsap.timeline().fromTo(".noticias>h1",{
+            gsap.timeline().fromTo(".scroll-titulo",{
                 x: "0%",
-            },{x:"-100%",
-              duration:5,
-              // repeat: -1,
+            },{x:"-1%",
+              duration:10,
+              repeat: -1,
               ease: "linear",
-          }),
+          })
         })
         return () => {
             ScrollTrigger.getAll().forEach(trigger => trigger.kill());
         };
     });
+    const spans = Array(100).fill("Noticias 뉴스 News ニュース Notizie 新聞 Новини");
+    const divs=Array(4).fill(0);
     return (
       <main className="noticias">
-        <h1>
+        <h1 className="scroll-titulo">
           {spans.map((text, index) => (<span key={index}>{text}</span>))}
         </h1>
+        <section className="noticias-parte">
+          <h1 className="noticias-titulo">Últimas Noticias</h1>
+          <div className="noticia">
+            {divs.map((index)=>(
+            <motion.div key={index} className="noticia-contenido">
+              <img src="" alt="" />
+              <p>artículo</p>
+              <h3>Estado del juego: esports de LoL en 2024</h3>
+            </motion.div>))}
+          </div>
+        </section>
       </main>
     )
   }
-  
   export default Noticias
