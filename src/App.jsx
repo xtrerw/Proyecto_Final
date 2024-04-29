e9937c5a577a3b454f78c257a3dbc5956090dde0
[Santiago Daza] <[santiagodaza22@gmail.com]>
2024-04-29 13:19:58 +0200

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 
 import './App.css'
-import Header from "../src/componentes/header";
 import Footer from "../src/componentes/footer";
 import Titulo from "./componentes_index/titulo";
 import How from './componentes_index/howToPlay';
 import Noticias from './componentes_index/noticias';
 import OurGames from './componentes_index/game';
 import Tienda from './componentes_index/tienda';
+import Navbar from './componentes/Navbar';
 // import { Routes,Route } from 'react-router-dom';
 function App() {
   return (
     <>
-      <Header/>
+      <Navbar/>
       <Titulo/>
       <How/>
       <OurGames/>
---
+++ src/componentes/MenuItems.jsx
@@ -0,0 +1,30 @@
+const menuItems = [
+    {
+      title: "Juegos",
+      url: "#",
+      cName: "nav-links"
+    },
+    {
+      title: "Tienda",
+      url: "#",
+      cName: "nav-links"
+    },
+    {
+      title: "Noticias",
+      url: "#",
+      cName: "nav-links"
+    },
+    {
+      title: "Torneos",
+      url: "#",
+      cName: "nav-links"
+    },
+    {
+      title: "Sign up",
+      url: "#",
+      cName: "nav-links-mobile"
+    }
+  ];
+  
+  export default menuItems;
+  
