ceebc6bac181b2dabb1c18c8c8e12d03d8b38911
wei <miguelxu124@.gmail.com>
2024-05-12 17:13:47 +0200

    fix api

--- src/PageTienda.jsx
+++ src/PageTienda.jsx
@@ -4,21 +4,21 @@ import TituloPaginas from './componentes_paginas/tituloPaginas';
 const Tienda = () => {
     const state={img:'src/img/bg2.png',title:"Tienda",description:"Nuestros regalos van más allá tu pensamiento"};
 
-    const[producto, setProducto]=useState();
+    const[tienda, setProducto]=useState();
     useEffect(()=>{
         fetch("https://localhost:3001/tienda")
         .then(response => response.json())
-        .then(producto =>{setProducto(producto)})
+        .then(tienda =>{setProducto(tienda)})
         .catch(error => {
             console.error('error:', error);
         });
     },[])
-    console.log(producto);
+    console.log(tienda);
   return (
     <>
     <TituloPaginas img={state.img} titulo={state.title} des={state.description}/>
     <main>
-
+        {}
     </main>
     </>
     
--- src/node/api.js
+++ src/node/api.js
@@ -6,22 +6,15 @@ const app = express();//crear una instancia de la Express y almacena en la "app"
 app.use(cors());//permiten intercambio de los datos entre diferentes dominios
 app.use(express.json()); //analizar cuerpo de las solicitudes, lo analizar'a como json 
 //api de noticias
-app.get('/noticias', async (req, res) => {
-  try {
-      const noticias = await ServerMod.NoticiasModulo.find({});
+app.get('/noticias', (req, res) => {
+
+      const noticias = ServerMod.NoticiasModulo.find({});
       res.json(noticias);
-  } catch (error) {
-      res.status(500).json({ message: 'error', error: error.toString() });
-  }
 });
 //api de tienda
-app.get('/tienda', async (req, res) => {
-  try {
-      const tienda= await ServerMod.TiendaModulo.find();
+app.get('/tienda', (req, res) => {
+      const tienda= ServerMod.TiendaModulo.find();
       res.json(tienda);
-  } catch (error) {
-      res.status(500).json({ message: 'error', error: error.toString() });
-  }
 });
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001;//configurar el n'umero de puerto. Intenta obtener el número puerto. Si no, se utilizará el puerto 3001
