fe9febfcee8e0a10f6dbb03cf161bd2cb2d366fe
[Santiago Daza] <[santiagodaza22@gmail.com]>
2024-05-12 14:26:06 +0200

    juegos

--- src/PageJuegos.jsx
+++ src/PageJuegos.jsx
@@ -5,7 +5,6 @@ import CardSearch from "./componentes_index/cardSearch";
 import Card from './componentes_index/gameCard'; // Importa el componente de Card
 
 export default function Games(){
-<<<<<<< HEAD
   const state={img:'src/img/bg3.png',title:"Our Games",description:"Descubre tu potencial en los juegos"};
 
   // Aquí puedes definir las URLs de tus imágenes para las cartas
@@ -24,9 +23,6 @@ export default function Games(){
     { id: 4, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
   ];
 
-=======
-  const state={img:'src/img/bg1.png',title:"Our Games",description:"Descubre tu potencial en los juegos"};
->>>>>>> 057784d50399164fb90264080a44df94bffc2b0e
   return (
     <div>
       <TituloPaginas img={state.img} titulo={state.title} des={state.description}/>
