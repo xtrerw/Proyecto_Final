0e078a2adecb11fc11f39ce3aff02b868b674925
wei xu <miguelxu124@gmail.com>
2024-05-12 20:43:59 +0200

    tienda

--- src/PageTienda.jsx
+++ src/PageTienda.jsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import TituloPaginas from './componentes_paginas/tituloPaginas';
 import "./PageTienda.css";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+
 const Tienda = () => {
     const state={img:'src/img/bg2.png',title:"Tienda",description:"Nuestros regalos van más allá tu pensamiento"};
 
-    const[tienda, setProducto]=useState([]);
+    const [tienda, setProducto]=useState([]);
     useEffect(()=>{
         fetch('http://localhost:3001/tienda')
         .then(response => response.json())
@@ -13,6 +17,25 @@ const Tienda = () => {
             console.error('error:', error);
         });
     },[]);
+    tienda.length
+    gsap.registerPlugin(ScrollTrigger);
+    useGSAP(()=>{
+      ScrollTrigger.create({
+        trigger:".page-tienda",
+        markers:true,
+        start:"0 80%",
+        end:"0 80%",
+        toggleActions: "restart none reverse",
+        animation:
+        gsap.timeline().fromTo(".productos",{
+          y:100,
+        },{
+          y:0,
+          duration:2,
+          ease: "expo.inOut",
+        }),
+      });
+    })
   return (
     <>
     <TituloPaginas img={state.img} titulo={state.title} des={state.description}/>
@@ -20,6 +43,9 @@ const Tienda = () => {
         <div className='productos'>
         {tienda.map(item => (
                     <div key={item._id} className='producto'>
+                        <div className='precio'>
+                          <p>{item.precio} ptos</p>
+                        </div>
                         <img src={item.img} />
                         <h3>{item.nombre}</h3>
                     </div>
