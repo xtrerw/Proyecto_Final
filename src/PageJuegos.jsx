52c437d1ab3b1cf571095cc761af0b3723714b9d
[Santiago Daza] <[santiagodaza22@gmail.com]>
2024-05-12 14:23:20 +0200

    juegos

--- src/PageJuegos.jsx
+++ src/PageJuegos.jsx
@@ -1,12 +1,37 @@
-import React from "react"
+import React from "react";
+import './componentes_index/gameCard.css';
 import TituloPaginas from './componentes_paginas/tituloPaginas';
+import CardSearch from "./componentes_index/cardSearch";
+import Card from './componentes_index/gameCard'; // Importa el componente de Card
 
 export default function Games(){
   const state={img:'src/img/bg3.png',title:"Our Games",description:"Descubre tu potencial en los juegos"};
+
+  // Aquí puedes definir las URLs de tus imágenes para las cartas
+  const cards = [
+    { id: 1, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 2, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 3, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 4, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 2, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 1, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 2, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 3, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 4, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 2, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 4, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 4, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+  ];
+
   return (
-    <TituloPaginas img={state.img} titulo={state.title} des={state.description}/>
-  )
+    <div>
+      <TituloPaginas img={state.img} titulo={state.title} des={state.description}/>
+      <CardSearch /> {/* Aquí se incorpora el componente de búsqueda */}
+      <div className="card-container">
+        {cards.map(card => (
+          <Card key={card.id} imageUrl={card.imageUrl} />
+        ))}
+      </div>
+    </div>
+  );
 }
-
-
-
---
+++ src/componentes_index/cardSearch.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CardSearch() {
+  return (
+    <div className="card-search">
+      <input type="text" placeholder="Buscar juego..." />
+      {/* Puedes agregar un botón de búsqueda si es necesario */}
+    </div>
+  );
+}
+
+export default CardSearch;
