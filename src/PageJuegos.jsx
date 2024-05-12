53aa7b967b6790ccd8a663f25c01ea20b6d3d5ce
[Santiago Daza] <[santiagodaza22@gmail.com]>
2024-05-12 14:24:52 +0200

    hola

--- src/PageJuegos.jsx
+++ src/PageJuegos.jsx
@@ -5,7 +5,28 @@ import CardSearch from "./componentes_index/cardSearch";
 import Card from './componentes_index/gameCard'; // Importa el componente de Card
 
 export default function Games(){
+<<<<<<< HEAD
   const state={img:'src/img/bg3.png',title:"Our Games",description:"Descubre tu potencial en los juegos"};
+
+  // Aquí puedes definir las URLs de tus imágenes para las cartas
+  const cards = [
+    { id: 1, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 2, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 3, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 4, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 2, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 1, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 2, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 3, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 4, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 2, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 4, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+    { id: 4, imageUrl: 'src/img/lol.png', to: '/pagina-de-destino-1' },
+  ];
+
+=======
+  const state={img:'src/img/bg1.png',title:"Our Games",description:"Descubre tu potencial en los juegos"};
+>>>>>>> 057784d50399164fb90264080a44df94bffc2b0e
   return (
     <div>
       <TituloPaginas img={state.img} titulo={state.title} des={state.description}/>
