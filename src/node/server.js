09b077bda61c3fcfbd152a1e76f28717ac044e32
wei xu <miguelxu124@gmail.com>
2024-05-14 13:01:56 +0200

    inserta productos

--- src/node/server.js
+++ src/node/server.js
@@ -5,7 +5,8 @@ import mongoose from 'mongoose';
 const user = encodeURIComponent("root"); // Usuario de MongoDB Atlas
 const password = encodeURIComponent("root"); // Contraseña del usuario
 const nombreBD = "OnlyGG"; // Nombre de la base de datos
-const url = `mongodb+srv://root:root@cluster0.ympghld.mongodb.net/${nombreBD}?retryWrites=true&w=majority&appName=Cluster0`;
+const url = `mongodb+srv://root:root@cluster0.ympghld.mongodb.net/${nombreBD}?retryWrites=true&w=majority&appName=Cluster0`;// para Daza
+const url2= `mongodb+srv://root:root@cluster0.3emmgzn.mongodb.net/${nombreBD}?retryWrites=true&w=majority&appName=Cluster0`;// para Wei
 
 mongoose.connect(url, {
     useNewUrlParser: true,
