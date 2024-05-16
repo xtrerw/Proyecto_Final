2081f65c41a835b54fe4a4737393261ffdca6860
wei xu <miguelxu124@gmail.com>
2024-05-16 12:49:30 +0200

    top premio

--- src/PageTienda.jsx
+++ src/PageTienda.jsx
@@ -5,6 +5,7 @@
   import gsap from "gsap";
   import {ScrollTrigger} from "gsap/ScrollTrigger";
   import { Link, Route, Routes } from 'react-router-dom';
+  import {Observer} from "gsap/Observer";
   const Tienda = () => {
       const state={img:'src/img/bg2.png',title:"Tienda",description:"Nuestros regalos van más allá tu pensamiento"};
 
@@ -42,7 +43,28 @@
             });
         }
     }, [tienda]);
-
+    //drag las cartas
+    gsap.registerPlugin(Observer,ScrollTrigger);
+    const premios=gsap.utils.toArray(".top-premio")
+    useEffect(() => {
+      // function move(xself) {
+        ScrollTrigger.create({
+          trigger: ".top-premio-parte",
+          scrub:2,
+          markers:true,
+          start: "40% 0%",
+          end: "40% -50%",
+          toggleActions: "restart none reverse none",
+          pin: true,
+          animation:
+          gsap.timeline().fromTo(premios,{
+            xPercent:100
+          },{
+            xPercent:-100 * (premios.length-2),
+          })
+        });
+        
+    }, [premios]); 
     return (
       <>
       <TituloPaginas img={state.img} titulo={state.title} des={state.description}/>
@@ -59,11 +81,20 @@
               ))}
           </div>
       </main>
-      <main className='top-premios'>
+      <main className="top-premios">
         <h1>Top Premios</h1>
+        <div className='top-premio-parte'>
+          {tienda.map((item,index)=>{
+            if (item.precio>200) {
+              return(
+              <div key={index} style={{backgroundImage:`url(src/img/premio${index}.png)`}} className="top-premio">
+                <img src={item.img} />
+              </div>)
+              }
+          })}
+        </div>
       </main>
       </>
-      
     )
   }
 
